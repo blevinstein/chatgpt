6f399d10fbcab668bb2b4a1b7ff46c0feb94dee6
Brian Levinstein <blevinstein@gmail.com>
2023-03-22 20:12:09 -0600

    Add basic file drop capability

--- whisper-gpt/static/image.js
+++ whisper-gpt/static/image.js
@@ -1,3 +1,13 @@
+const blobToBase64 = blob => {
+    const reader = new FileReader();
+    reader.readAsDataURL(blob);
+    return new Promise(resolve => {
+        reader.onloadend = () => {
+            resolve(reader.result);
+        };
+    });
+};
+
 document.addEventListener('DOMContentLoaded', async () => {
     registerAudioButtons();
     registerChatControls();
@@ -6,6 +16,29 @@ document.addEventListener('DOMContentLoaded', async () => {
 
     // Display server build time:
     await fetchBuildTime();
+
+    const updateImage = async (event) => {
+        event.preventDefault();
+
+        window.event = event;
+
+        let file;
+        if (event.dataTransfer.items && event.dataTransfer.items.length > 0) {
+            file = event.dataTransfer.items[0].getAsFile();
+        } else if(event.dataTransfer.files && event.dataTransfer.files.length > 0) {
+            file = event.dataTransfer.files[0];
+        }
+        if (!file) {
+            console.error('No file data:', event);
+            return;
+        }
+
+        const img = cloneTemplate('imageSubject');
+        img.src = await blobToBase64(file);
+        document.getElementById('imageContainer').children[0].replaceWith(img);
+    };
+    document.getElementById('imageContainer').ondrop = updateImage;
+    document.getElementById('imageContainer').ondragover = (event) => event.preventDefault();
 
     // If requested, load chat state from log
     const queryParams = new Map(new URLSearchParams(window.location.search).entries());
